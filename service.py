aa89144f21fcb07c62a0829a65ca08075cb6aacf
Rayer Tung <Rayer.Tung@innovasolutions.local>
2018-03-22 22:40:20 -0500

    Add PID Lock

---
+++ service.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+
+def pid_lock(pid_file, main_logic):
+    pid = str(os.getpid())
+    if os.path.isfile(pid_file):
+        with open(pid_file, 'r') as file:
+            old_pid = file.read(6)
+        print('Service is already running at {0}, deleting this file and restart'.format(old_pid))
+        try:
+            os.kill(int(old_pid), 9)
+        except OSError as e:
+            print('Error deleting PID {0} : {1}'.format(old_pid, e.strerror))
+        finally:
+            os.remove(pid_file)
+
+    with open(pid_file, 'w') as file:
+        print('Start application at PID {0}'.format(pid))
+        file.write(pid)
+
+    try:
+        main_logic()
+    finally:
+        os.remove(pid_file)
+
